574f15761a655ec4d39056330a44779ccad32752
Touriste <legrandgustave2001@gmail.com>
2024-02-11 19:52:15 +0100

    Test to IPFS

---
+++ build/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    // Send the index.html file when someone accesses the root URL
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
